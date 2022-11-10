1afd5e821d2ce9670bb189bb22b4bf43bdd0a684
Константин <asistent0@gmail.com>
2022-11-10 21:57:40 +0300

    rename func print to mePrint

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	print(maths.Sum, 2, 3)
-	print(maths.Sub, 5, 3)
+	myPrint(maths.Sum, 2, 3)
+	myPrint(maths.Sub, 5, 3)
 }
 
-func print(f func(int, int) int, a int, b int) {
+func myPrint(f func(int, int) int, a int, b int) {
 	fmt.Println(f(a, b))
 }
