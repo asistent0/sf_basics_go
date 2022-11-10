194dca161929d183760409b343862b5e6a62193a
Константин <asistent0@gmail.com>
2022-11-10 22:29:14 +0300

    add game

--- main.go
+++ main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/asistent0/sf_basics_go/maths"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	myPrint(maths.Sum, 2, 3)
-	myPrint(maths.Sub, 5, 3)
-	myPrint(func(a int, b int) int { return a * b }, 5, 3)
-}
+	rand.Seed(time.Now().Unix())
+	target := rand.Intn(100) + 1
 
-func myPrint(f func(int, int) int, a int, b int) {
-	fmt.Println(f(a, b))
+	fmt.Println("Я загадал случайное число от 1 до 100")
+	fmt.Println("Угадаешь его?")
+
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 10; guesses > 0; guesses-- {
+		fmt.Println("У тебя осталось", guesses, "попыток")
+
+		fmt.Println("Введи новое число: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess < target {
+			fmt.Println("Ой. Загаданное число больше введенного")
+		} else if guess > target {
+			fmt.Println("Ой. Загаданное число меньше введенного")
+		} else {
+			fmt.Println("Ура! Загаданное число угадано!!!")
+			success = true
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Ты не смог угадать число... :-(")
+	}
 }
