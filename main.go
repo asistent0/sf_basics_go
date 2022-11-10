079ae7d94df3412e53edda8fe094e1a442f2c569
Константин <asistent0@gmail.com>
2022-11-10 21:54:44 +0300

    add print(f, a, b)

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(maths.Sum(2, 3))
-	fmt.Println(maths.Sub(5, 3))
+	print(maths.Sum, 2, 3)
+	print(maths.Sub, 5, 3)
+}
+
+func print(f func(int, int) int, a int, b int) {
+	fmt.Println(f(a, b))
 }
