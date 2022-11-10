14097ff9bc8be92080e5aa83bf55423660e98b97
Константин <asistent0@gmail.com>
2022-11-10 22:09:02 +0300

    add multiplication

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	myPrint(maths.Sum, 2, 3)
 	myPrint(maths.Sub, 5, 3)
+	myPrint(func(a int, b int) int { return a * b }, 5, 3)
 }
 
 func myPrint(f func(int, int) int, a int, b int) {
